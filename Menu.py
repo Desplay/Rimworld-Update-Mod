83815dc436cb652e73a639c158a95503fd921958
Desplay Shido <101574256+Desplay@users.noreply.github.com>
2024-03-13 16:35:01 +0700

    Add Menu

---
+++ Menu.py
@@ -0,0 +1,19 @@
+from InstallMods import Handling as InstallMods
+from UpdateMods import Handling as UpdateMods
+
+def Menu():
+    print("1. Install Mods")
+    print("2. Update Mods")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        InstallMods()
+    elif choice == "2":
+        UpdateMods()
+    elif choice == "3":
+        exit()
+    else:
+        print("Invalid choice")
+        Menu()
+        
+Menu()
--- UpdateMods.py
+++ UpdateMods.py
@@ -1,104 +1,11 @@
 import os
 import subprocess
-from datetime import datetime
-import urllib.request
-
-""" Get game directory """
-gameDir = os.getcwd()
-gameId = open(os.path.join(gameDir, "SWconfig.ini"), "r").read().split("Appid = ")[1].split("\n")[0]
-steamcmd = os.path.join(gameDir, "steamcmd", "steamcmd.exe")
-Modfolder = os.path.join(gameDir, "LinkNeverDie.Com-GSE", "mods")
-ModRootFolder = os.path.join(gameDir, "steamcmd", "steamapps", "workshop", "content", gameId)
-
-""" Get all mod in folder """
-mods = [
-    name
-    for name in os.listdir(ModRootFolder)
-    if os.path.isdir(os.path.join(ModRootFolder, name))
-]
-
-""" unlink mod folder """
-def unlink():
-    try:
-        os.unlink(Modfolder)
-    except:
-        pass
-unlink()
-
-def is_valid_date(date_str, date_format):
-    try:
-        datetime.strptime(date_str, date_format)
-        return True
-    except ValueError:
-        return False
-
-
-""" Get last update mod from steam workshop web with mod id """
-
-
-def getLastUpdate(modId):
-    fp = urllib.request.urlopen(
-        "https://steamcommunity.com/sharedfiles/filedetails/?id=" + modId
-    )
-    mybytes = fp.read()
-    mystr = mybytes.decode("utf8")
-    fp.close()
-    try:
-        mystr = (
-            mystr.split('<div class="detailsStatRight">')[3].split("</div>")[0].strip()
-        )
-        return mystr
-    except:
-        return "0"
-
-
-""" get current version mod """
-
-
-def getCurrentVersion(mod):
-    mod_version_file = os.path.join(
-        gameDir,
-        "steamcmd",
-        "steamapps",
-        "workshop",
-        "content",
-        "294100",
-        mod,
-        "About",
-        "timestamp.txt",
-    )
-    if os.path.exists(mod_version_file) == False:
-        mod_version = 0
-    else:
-        mod_version = open(mod_version_file, encoding="utf8").read()
-    return mod_version
-
-
-""" Check mod version """
-
-
-def formatDate(current, dateFromWeb):
-    if current == 0:
-        return True
-    if is_valid_date(current, "%d/%m/%Y %H:%M:%S"):
-        date1 = datetime.strptime(current, "%d/%m/%Y %H:%M:%S")
-    elif is_valid_date(current, "%m/%d/%Y %I:%M:%S %p"):
-        date1 = datetime.strptime(current, "%m/%d/%Y %I:%M:%S %p")
-    elif is_valid_date(current, "%d %b, %Y @ %I:%M%p"):
-        date1 = datetime.strptime(current, "%d %b, %Y @ %I:%M%p")
-    elif is_valid_date(current, "%d %b @ %I:%M%p"):
-        date1 = datetime.strptime(current, "%d %b @ %I:%M%p")
-    elif is_valid_date(current, "%Y-%m-%d %H:%M:%S"):
-        date1 = datetime.strptime(current, "%Y-%m-%d %H:%M:%S")
-
-    if is_valid_date(dateFromWeb, "%d %b, %Y @ %I:%M%p"):
-        date2 = datetime.strptime(dateFromWeb, "%d %b, %Y @ %I:%M%p")
-    if is_valid_date(dateFromWeb, "%d %b @ %I:%M%p"):
-        date2 = datetime.strptime(dateFromWeb, "%d %b @ %I:%M%p")
-    return date1 < date2
+from Handling import *
 
 
 """ update mod """
+
+unlink()
 
 
 def update_mods(modslist):
@@ -170,37 +77,34 @@ def Handling():
             )
             print(m.split("to")[0] + "( " + modname + " )" + " successfully ! \n")
     if not NOT_ERROR:
-        logFile.write("please update error mod manual" + "\n")
+        logFile.write("please update error mod manual or try again this app" + "\n")
         logFile.write("list mod error: \n")
         for mod in modError:
             logFile.write(mod + "\n")
         print("please update error mod manual" + "\n")
         print("list mod erroris in log file: logs.txt \n")
-    if NOT_ERROR:
-        logFile.write("All mod is updated\n list mod: \n")
-        for mod in mods:
-            mod_About = os.path.join(
-                gameDir,
-                "steamcmd",
-                "steamapps",
-                "workshop",
-                "content",
-                "294100",
-                mod,
-                "About",
-                "About.xml",
-            )
-            openFile = open(mod_About, encoding="utf8").read()
-            mod_name = openFile.split("</name>")[0].split("<name>")[1]
-            logFile.write(mod + ": " + mod_name + "\n")
+    logFile.write("All mod is updated\n list mod: \n")
+    for mod in mods:
+        mod_About = os.path.join(
+            gameDir,
+            "steamcmd",
+            "steamapps",
+            "workshop",
+            "content",
+            "294100",
+            mod,
+            "About",
+            "About.xml",
+        )
+        openFile = open(mod_About, encoding="utf8").read()
+        mod_name = openFile.split("</name>")[0].split("<name>")[1]
+        logFile.write(mod + ": " + mod_name + "\n")
     for m in message:
         rawLogFile.write(m + "\n")
     logFile.close()
     rawLogFile.close()
     os.symlink(
         os.path.join(gameDir, "steamcmd", "steamapps", "workshop", "content", "294100"),
-        os.path.join(gameDir, "LinkNeverDie.Com-GSE", "mods"),
+        Modfolder,
     )
-
-
-Handling()
+    input("Press Enter to continue...")
